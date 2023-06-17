48046e960137aa127fee223f969ccac145e4660e
lucas github <ld@ld.rip>
2023-06-17 17:00:33 -0500

    LDOS init

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("LDOS")
+const desktop = document.getElementById("desktop")
+const ldBar = document.getElementById("LDbar")
+const programs = document.getElementById("programs")
+const terminal = document.getElementById("terminal")
+ldBar.innerText = "this is where the bar is going to be";
