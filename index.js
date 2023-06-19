ccfe9e13eaec945461d0a33e06769652a06c2200
lucas github <ld@ld.rip>
2023-06-18 22:37:38 -0500

    fixed terminal a bit

--- index.js
+++ index.js
@@ -5,6 +5,34 @@ const programs = document.getElementById("programs");
 const terminal = document.getElementById("terminal");
 const searchBar = document.getElementById("searchbar");
 const inputBar = document.getElementById("inputBar");
+let fileSystem = {
+  name: "root",
+  type: "directory",
+  children: {
+    home: {
+      name: "home",
+      type: "directory",
+      children: {
+        user: {
+          name: "user",
+          type: "directory",
+          children: {},
+        },
+      },
+    },
+    coolstuff: {
+      name: "coolstuff",
+      type: "directory",
+      children: {
+        testFile: {
+          name: "testFile",
+          type: "file",
+          children: {},
+        },
+      },
+    },
+  },
+};
 let processes = 0;
 const apps = [
   "terminal",
@@ -22,6 +50,8 @@ window.addEventListener("keydown", (event) => {
     } else {
       closeSearchBar();
     }
+  } else if (searchBar.style.display === "flex" && event.key === "Escape") {
+    closeSearchBar();
   }
 });
 
@@ -37,18 +67,83 @@ inputBar.addEventListener("keydown", function (event) {
   }
 });
 
-function interpretSearchValue(value) {
+async function interpretSearchValue(value) {
+  await sleep(750);
   if (value === "terminal") {
     openTerminal();
   }
 }
 
 function openTerminal() {
-  terminal.style.width = window.innerWidth - 20 + "px";
-  terminal.style.height = window.innerHeight - 60 + "px";
-  terminal.style.display = "block";
+  tileProcess(terminal);
   const terminalText = document.getElementById("terminaltext");
   terminalText.focus();
+  terminalText.addEventListener("input", (event) => {
+  });
+  terminalText.addEventListener("keydown", (enter) => {
+    if (enter.key === "Enter") {
+      interpretTerminalCommand(terminalText.value);
+      terminalText.value = "";
+    }
+  });
+
+  terminal.addEventListener("click", () => {
+    terminalText.focus();
+  });
+}
+
+function cd(directory, currentDirectory) {
+  if (directory === "..") {
+    if (currentDirectory.name !== "/") {
+      currentDirectory = currentDirectory.parent;
+    }
+  }
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function interpretTerminalCommand(command) {
+  const terminalOutput = document.getElementById("terminaloutput");
+  let ps1 = document.getElementById("ps1");
+  let clone = ps1.cloneNode(true);
+  document.getElementById("terminaloutput").appendChild(clone);
+
+  if (command === "hi") {
+    commandOutput("hello");
+  } else if (command === "clear") {
+    terminalOutput.innerHTML = "";
+  } else if (command === "whoami") {
+    commandOutput("idfk");
+  } else {
+    commandOutput("");
+  }
+}
+
+function commandOutput(outputs) {
+  const terminalOutput = document.getElementById("terminaloutput");
+  terminalOutput.innerHTML += "<br>" + outputs + "<br>";
+}
+
+function tileProcess(process) {
+  process.style.display = "block";
+  let width = 0;
+  let height = 0;
+  console.log(window.innerWidth);
+  let getBigger = setInterval(function () {
+    if (width > window.innerWidth - 21 || height > window.innerHeight - 21) {
+      clearInterval(getBigger);
+      process.style.width = window.innerWidth - 20 + "px";
+      process.style.height = window.innerHeight - 60 + "px";
+    } else {
+      process.style.width = width + "px";
+      process.style.height = height + "px";
+      width += 20;
+      height += 20;
+      console.log("hi");
+    }
+  }, 3);
 }
 
 function openSearchBar() {
